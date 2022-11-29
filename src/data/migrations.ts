a935342bca79cb5e6cecd3845ba72685b7045450
Rafael Castilho de Castro <raphaeldavant@hotmail.com>
2022-11-29 19:49:10 -0300

    Feat: Add migration.ts

---
+++ src/data/migrations.ts
@@ -0,0 +1,50 @@
+import { connection } from "./connection"
+
+const printError = (error: any) => { console.log(error.sqlMessage || error.message) }
+
+const tableUsers = 'labecommerce_users'
+const tableProducts = 'labecommerce_products'
+
+const createTables = () => 
+    connection.schema
+        .hasTable(tableUsers).then((exists)=>{
+            if (!exists) {
+                const table = connection.schema
+                .createTable(tableUsers, (table) =>{
+                    table.primary(['id']);
+                    table.string('id');
+                    table.string('name');
+                    table.string('email');
+                    table.string('password');
+                })
+                console.log(`Table ${tableUsers} created.`)
+                return table
+            } else {
+                console.log(`Table ${tableUsers} already exists.`)
+            }
+        })
+        .catch(printError)
+
+    connection.schema
+        .hasTable(tableProducts).then((exists)=>{
+            if (!exists) {
+                const table = connection.schema
+                    .createTable(tableProducts, (table) =>{
+                        table.primary(['id']);
+                        table.string('id');
+                        table.string('name');
+                        table.integer('price');
+                        table.string('image_url');
+                    })
+                console.log(`Table ${tableProducts} created.`)
+                return table
+            } else {
+                console.log(`Table ${tableProducts} already exists.`)
+            }
+        })
+        .catch(printError)
+
+const closeConnection = () => { connection.destroy() }
+
+createTables()
+    .finally(closeConnection)
